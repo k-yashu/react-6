1194f767a152548977bd988c9e1eeea7ee938f16
k-yashu <kyashu2003@gmail.com>
2023-10-14 23:29:46 +0530

    first commit

---
+++ src/Portfolio.js
@@ -0,0 +1,167 @@
+import React from 'react';
+
+ 
+
+const Portfolio = () => (
+
+  <div className="portfolio">
+
+    <div className="header">
+
+      <h1>K YASASWINI</h1>
+
+      <h2>Student VIT AP</h2>
+
+      <div className="contact-info">
+
+        <h3>
+
+          <a href="mailto:kyashu2003@gmail.com">kyashu2003@gmail.com</a>
+
+        </h3>
+
+        <h3>9347467402</h3>
+
+      </div>
+
+    </div>
+
+ 
+
+    <div className="about">
+
+      <h2>About</h2>
+
+      <p className="enlarge">
+
+        I am VIT AP Student<br />
+
+        Born on 4-september-2003<br />
+
+        I have taken CSE (ai & ml)<br />
+
+        My registration number--21bce7378
+
+      </p>
+
+    </div>
+
+ 
+
+    <div className="education">
+
+      <h2>Education</h2>
+
+      <div className="education-item">
+
+        <h2>maharishi vidya mandir - Hyderabad, India</h2>
+
+        <p>10th class 9.2 GPA</p>
+
+      </div>
+
+      <div className="education-item">
+
+        <h2>sri Chaitanya kalasala - Hyderabad, India</h2>
+
+        <p>Inter A Grade</p>
+
+      </div>
+
+    </div>
+
+ 
+
+    <div className="at-vit">
+
+      <h2>AT VIT AP</h2>
+
+      <p className="enlarge">
+
+        I am a third year student.
+
+      </p>
+
+    </div>
+
+ 
+
+    <div className="skills">
+
+      <h2>Skills</h2>
+
+      <div className="talent">
+
+        <h2>Python</h2>
+
+        <p>Core and Advanced Python specialist</p>
+
+      </div>
+
+      <div className="talent">
+
+        <h2>JavaScript</h2>
+
+        <p>Can create complex web application front end scripting using JS and jQuery</p>
+
+      </div>
+
+      <div className="talent">
+
+        <h2>HTML</h2>
+
+        <p>Strong in writing HTML for UI design</p>
+
+        <br></br>
+
+        <br></br>
+
+      </div>
+
+    </div>
+
+    <div className="interests">
+
+      <h2>Interests and Hobbies</h2>
+
+      <ul className="talent">
+
+        <li>Reading Books</li>
+
+        <li>Playing Cricket</li>
+
+      </ul>
+
+      <ul className="talent">
+
+        <li>Coding</li>
+
+        <li>Watching Movies</li>
+
+      </ul>
+
+    </div>
+
+ 
+
+<br></br>
+
+<br></br>
+
+<br></br>
+
+<br></br>
+
+    <div className="footer">
+
+      <p>anjali tadikonda &mdash; <a href="mailto:kyashu2003@gmail.com">kyashu2003@gmail.com</a> &mdash; 9347467402</p>
+
+    </div>
+
+  </div>
+
+);
+
+ 
+
+export default Portfolio;
